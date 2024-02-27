06174072a106cebf71f3dc908ab5e0f7ec3cde5c
Raghvendra <raghvendra14bhardwaj@gmail.com>
2024-02-28 03:14:38 +0530

    Fixed refresh issue

--- src/Data/Data.ts
+++ src/Data/Data.ts
@@ -5,13 +5,15 @@ import { UserDetail } from "../Interface";
 
 export const getUserDetails = async () => {
   try {
-    let userDetails: UserDetail = defaultUserDetails;
     const response: any = await axios.get(API_URL);
-    userDetails.title = response.data.results[0].name.title;
-    userDetails.firstName = response.data.results[0].name.first;
-    userDetails.lastName = response.data.results[0].name.last;
-    userDetails.email = response.data.results[0].email;
-    userDetails.image = response.data.results[0].picture.large;
+    const userData = response.data.results[0];
+    const userDetails: UserDetail = {
+      title: userData.name.title,
+      firstName: userData.name.first,
+      lastName: userData.name.last,
+      email: userData.email,
+      image: userData.picture.large
+    };
     return userDetails;
   } catch (error) {
     throw new Error("Error while fetch data!!!");
