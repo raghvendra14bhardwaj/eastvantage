07c7bc8d3a43c23ac709d7fe35bc24b37425c415
RAGHVENDRA BHARDWAJ <raghvendra14bhardwaj@gmail.com>
2024-02-28 03:37:14 +0530

    Removed routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
 import Home from "./Components/Home/Home";
 import "./App.css";
 
 function App() {
   return (
     <div className="main">
-      <Routes>
-        <Route path="*" element={<Home />} />
-      </Routes>
+     <Home />
     </div>
   );
 }
